dd4ae365f7eaba84454928a84edb5c78fa71631e
Willian Oliveira <willian.dev.tech@gmail.com>
2026-03-25 18:45:23 -0300

    feat: update authentication configuration and add session expiration

--- src/lib/auth/auth.lib.ts
+++ src/lib/auth/auth.lib.ts
@@ -1,31 +1,35 @@
-import { env } from '@/config/env.js';
-import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { betterAuth } from 'better-auth';
+import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { openAPI } from 'better-auth/plugins';
 import { localization } from 'better-auth-localization';
+import { env } from '@/config/env.js';
 import * as schema from '@/lib/db/schemas/index.schema.js';
 import { db } from '../db/connection.js';
 
 export const auth = betterAuth({
-    baseURL: env.BETTER_AUTH_URL,
-    secret: env.BETTER_AUTH_SECRET,
-    trustedOrigins: env.ALLOWED_ORIGINS,
-    advanced: {
-        disableOriginCheck: env.NODE_ENV !== 'production',
-    },
-    socialProviders: {
-        google: {
-            clientId: env.GOOGLE_CLIENT_ID,
-            clientSecret: env.GOOGLE_CLIENT_SECRET,
-        },
-        linkedin: {
-            clientId: env.LINKEDIN_CLIENT_ID,
-            clientSecret: env.LINKEDIN_CLIENT_SECRET,
-        },
-    },
-    database: drizzleAdapter(db, {
-        provider: 'pg',
-        schema: schema,
-    }),
-    plugins: [openAPI(), localization({ defaultLocale: 'pt-BR' })],
+	baseURL: env.BETTER_AUTH_URL,
+	secret: env.BETTER_AUTH_SECRET,
+	trustedOrigins: env.ALLOWED_ORIGINS,
+	advanced: {
+		disableOriginCheck: env.NODE_ENV !== 'production',
+	},
+	socialProviders: {
+		google: {
+			clientId: env.GOOGLE_CLIENT_ID,
+			clientSecret: env.GOOGLE_CLIENT_SECRET,
+		},
+		linkedin: {
+			clientId: env.LINKEDIN_CLIENT_ID,
+			clientSecret: env.LINKEDIN_CLIENT_SECRET,
+		},
+	},
+	database: drizzleAdapter(db, {
+		provider: 'pg',
+		schema: schema,
+	}),
+	session: {
+		expiresIn: 60 * 60 * 24 * 7,
+	},
+
+	plugins: [openAPI(), localization({ defaultLocale: 'pt-BR' })],
 });
